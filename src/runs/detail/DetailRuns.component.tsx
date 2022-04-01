9780f4eff6e4f073f5090ddd46ae5412a2c02780
Yvann Butticaz <34337152+Vidivanze@users.noreply.github.com>
2022-04-01 10:42:10 +0200

    Add if run is updated or new add c'est noté btn

--- src/runs/detail/DetailRuns.component.tsx
+++ src/runs/detail/DetailRuns.component.tsx
@@ -1,5 +1,5 @@
 import {ScrollView} from "react-native";
-import React, {Fragment} from "react";
+import React, {Fragment, useState, useEffect} from "react";
 import {DetailRunsScheduleComponent} from "./DetailRunsSchedule.component";
 import {DetailRunsCourseComponent} from "./DetailRunsCourse.component";
 import {DetailRunsInfoComponent} from "./DetailRunsInfo.component";
@@ -8,21 +8,60 @@ import {DetailRunsContactBtn} from "./DetailRunsContactBtn.component";
 import {DetailRunsStatusControlBtn} from "./DetailRunsStatusControlBtn";
 import {DetailRunsAcknowledgeUpdateComponent} from "./DetailRunsAcknowledgeUpdate.component";
 import {useRunFromRouteParam} from "../../common/hook/Run.hook";
-import {lastUpdatedRun} from "../../common/utils/LastUpdatedRun.utils";
+import {lastUpdatedRun, runIsMine, runIsNew, detailsChanges, checkScheduleChanges, checkWaypointsChanges, checkInfoChanges, checkRunnersChanges } from "../../common/utils/MyRuns";
 import {AuthContainer} from "../../Provider.component";
 
 export function DetailRunsComponent() {
     const currentRun = useRunFromRouteParam();
     const {authenticatedUser} = AuthContainer.useContainer();
+    const [isNewOrUpdated, setIsNewOrUpdated] = useState<boolean>();
+    const [isMine, setIsMine] = useState<boolean>();
+    
+    if(currentRun){
+        detailsChanges(currentRun!);
 
-    if (!currentRun) {
+        async function checkIsNewOrUpdated() {
+            setIsNewOrUpdated(((await runIsNew(currentRun!, authenticatedUser?.id) && runIsMine(currentRun!, authenticatedUser?.id))));
+        }
+
+        async function checkIsMine() {
+            setIsMine(await runIsMine(currentRun!, authenticatedUser?.id));
+        }
+
+        useEffect(() => {
+            const promise = checkIsNewOrUpdated();
+
+            return () =>{
+                Promise.reject(promise);
+            }
+        }, []);
+
+        useEffect(() => {
+            const promise = checkIsMine();
+
+            return () =>{
+                Promise.reject(promise);
+            }
+        }, []);
+
+        if (typeof isNewOrUpdated === "undefined") {
+            return null;
+        }
+
+        if (typeof isMine === "undefined") {
+            return null;
+        }
+    
+    }else {
         console.error("No run matching provided found for provided run id ")
         return <Fragment/>;
     }
+
     
     return (
-        <ScrollView style={{backgroundColor: 'white'}}>                         
-            {lastUpdatedRun(currentRun, authenticatedUser?.id) ? <DetailRunsAcknowledgeUpdateComponent currentRun={currentRun}/> : false }
+        <ScrollView style={{backgroundColor: 'white'}}>
+                   
+            {isNewOrUpdated ? <DetailRunsAcknowledgeUpdateComponent setIsNewOrUpdated={setIsNewOrUpdated} currentRun={currentRun}/> : false}
 
             <DetailRunsStatusControlBtn currentRun={currentRun}/>
 
