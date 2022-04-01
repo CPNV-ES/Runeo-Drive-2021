497224af648c91afda4babb2625da4780e7102ee
Yvann Butticaz <34337152+Vidivanze@users.noreply.github.com>
2022-04-01 10:33:31 +0200

    Check if runIsNewOrUpdated then display ** New ** and run is highlighted

--- src/runs/list/ListRunsItem.component.tsx
+++ src/runs/list/ListRunsItem.component.tsx
@@ -1,10 +1,10 @@
 import {getRunStatusIcon} from "../../common/utils/Run.utils";
 import {ListItem} from "react-native-elements";
 import {DATE_FORMAT} from "../../common/utils/Date.utils";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import {RunResource} from "../../common/resources/Run.resource";
 import {Colors} from "../../common/utils/Color.utils";
-import {lastUpdatedRun} from "../../common/utils/LastUpdatedRun.utils";
+import {lastUpdatedRun, runIsMine, runIsNew} from "../../common/utils/MyRuns";
 import {AuthContainer} from "../../Provider.component";
 
 export type ListRunsItemComponentProps = {
@@ -14,14 +14,48 @@ export type ListRunsItemComponentProps = {
 
 //use a react PureComponent to limit number of render when used in animated flat list
 export function ListRunsItemComponent ({onSelectRun, run} : ListRunsItemComponentProps) {
-    
     const {authenticatedUser} = AuthContainer.useContainer();
-    
+
+    const [isNewOrUpdated, setIsNewOrUpdated] = useState<boolean>();
+    const [isNew, setIsNew] = useState<boolean>();
+
+    async function checkIsNewOrUpdated() {
+        setIsNewOrUpdated(((await runIsNew(run, authenticatedUser?.id) && runIsMine(run, authenticatedUser?.id)) || lastUpdatedRun(run, authenticatedUser?.id)));
+    }
+
+    async function checkIsNew() {
+        setIsNew((await runIsNew(run, authenticatedUser?.id) && runIsMine(run, authenticatedUser?.id)));
+    }
+
+    useEffect(() => {
+        const promise = checkIsNewOrUpdated();
+
+        return () =>{
+            Promise.reject(promise);
+        }
+    }, []);
+
+    useEffect(() => {
+        const promise = checkIsNew();
+
+        return () =>{
+            Promise.reject(promise);
+        }
+    }, []);
+
+    if (typeof isNewOrUpdated === "undefined") {
+        return null;
+    }
+
+    if (typeof isNew === "undefined") {
+        return null;
+    }
+
     return (
-        <ListItem bottomDivider onPress={() => onSelectRun(run)} containerStyle={ lastUpdatedRun(run, authenticatedUser?.id) ? {backgroundColor: Colors.GREEN} : false}>
+        <ListItem bottomDivider onPress={() => onSelectRun(run)} containerStyle={ isNewOrUpdated ? {backgroundColor: Colors.GREEN} : false}>
             {getRunStatusIcon(run.status)}
             <ListItem.Content>
-                <ListItem.Title style={{fontFamily: 'Montserrat-Medium'}}>{`${run.title.toUpperCase()} ${run.waypoints.get(0)?.nickname}`}</ListItem.Title>
+                <ListItem.Title style={{fontFamily: 'Montserrat-Medium'}}>{isNew ? "**New** " : ''}{`${run.title.toUpperCase()} ${run.waypoints.get(0)?.nickname}`}</ListItem.Title>
                 <ListItem.Subtitle style={{color: Colors.GREY, fontFamily: 'Montserrat-Regular'}}>{
                     run
                         .begin_at
