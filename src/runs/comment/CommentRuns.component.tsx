76a2d15a92ee89b7f827234fed627b4a71ef687d
Nomeos <mathieu.rabot01@gmail.com>
2022-06-03 10:45:10 +0200

    Create CommentRuns.component.tsx that is the log page

---
+++ src/runs/comment/CommentRuns.component.tsx
@@ -0,0 +1,14 @@
+import React, {useState} from "react";
+import {SafeAreaView, View, Text} from "react-native";
+
+
+
+export function CommentRunsComponent() {
+    return (
+        <SafeAreaView>
+            <View>
+                <Text>Hello World</Text>
+            </View>
+        </SafeAreaView>
+    );
+}
