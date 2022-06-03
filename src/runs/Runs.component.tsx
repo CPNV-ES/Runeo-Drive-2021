f3741b4d75d563848752569040925b64a8e1f0ff
Nomeos <mathieu.rabot01@gmail.com>
2022-06-03 10:43:49 +0200

    Import the new component view

--- src/runs/Runs.component.tsx
+++ src/runs/Runs.component.tsx
@@ -6,6 +6,7 @@ import {DetailRunsComponent} from "./detail/DetailRuns.component";
 import {RunsContainer} from "../Provider.component";
 import {RunsSelectVehicleComponent} from "./RunsSelectVehicle.component";
 import {RunsEndComponent} from "./RunsEnd.component";
+import {CommentRunsComponent} from "./comment/CommentRuns.component";
 
 const Stack = createStackNavigator();
 
@@ -45,6 +46,11 @@ export function RunsComponent() {
                                   headerBackTitle: "Annuler"
                               }
                           }}/>
+            <Stack.Screen
+                name={"comment"}
+                component={CommentRunsComponent}
+                options={{headerTitle: "Commentaires"}}
+            />
         </Stack.Navigator>
     )
 }
