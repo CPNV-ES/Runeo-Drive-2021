6ed21c739717998fb66c9cc21f9c987ebe8a0ca6
Nomeos <mathieu.rabot01@gmail.com>
2022-06-13 16:40:59 +0200

    Add the form fragment to the view

--- src/runs/comment/CommentRuns.component.tsx
+++ src/runs/comment/CommentRuns.component.tsx
@@ -1,9 +1,10 @@
-import React, {useState} from "react";
+import React, {Fragment, useState} from "react";
 import {ScrollView} from "react-native";
 import {LogsDetailRunComponent} from "./LogsDetailRun.component";
 import {useRoute} from "@react-navigation/native";
 import {RunsContainer} from "../../Provider.component";
 import {RunDetailParams} from "../Runs.component";
+import {CreateLogDetailRunsComponent} from "./CreateLogDetailRun.component";
 
 export function CommentRunsComponent() {
     const runsContainer = RunsContainer.useContainer();
@@ -11,11 +12,17 @@ export function CommentRunsComponent() {
     const route = useRoute();
     const {runId} = route.params as RunDetailParams;
 
-    const currentRun = runsContainer.items.find(run => run.id === runId)
+    const currentRun = runsContainer.items.find(run => run.id === runId);
+
+    if (!currentRun) {
+        console.error("No run matching provided run id " + runId);
+        return <Fragment/>;
+    }
 
     return (
         <ScrollView style={{backgroundColor: 'white'}}>
-            <LogsDetailRunComponent currentRun={currentRun}/>
+
+            <CreateLogDetailRunsComponent currentRun={currentRun}/>
         </ScrollView>
     );
 }
