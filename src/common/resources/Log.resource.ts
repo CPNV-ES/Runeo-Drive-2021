f9dffe6dfa75a140117ba637e0a94ec46e5e988e
Nomeos <mathieu.rabot01@gmail.com>
2022-06-25 22:12:52 +0200

    Update Log.resource.ts

--- src/common/resources/Log.resource.ts
+++ src/common/resources/Log.resource.ts
@@ -4,7 +4,7 @@ import {CommonResource} from "./Common.resource";
 export interface LogResource extends CommonResource {
     updated_at: string,
     description: string,
-    created_at: string,
-    action: string
+    action: string,
+    id: number,
 }
 
