fb8cee65c63e9b07504e5778a0d8141c31c9b243
Nomeos <mathieu.rabot01@gmail.com>
2022-06-26 19:47:53 +0200

    Display current user's name

--- src/params/DisplayName.component.tsx
+++ src/params/DisplayName.component.tsx
@@ -1,11 +1,23 @@
 import React from "react";
+import { View, Text } from "react-native";
+import {AuthContainer, UsersContainer} from "../Provider.component";
 
 
 export function DisplayNameComponent() {
 
-    /*return (
+    /**
+     * Get current user from the container
+     * @type {UsersContainer}
+     */
+    const authContainer = AuthContainer.useContainer();
+    const user = authContainer.authenticatedUser;
 
-    )*/
+
+    return (
+        <View>
+            <Text>Votre nom d'affichage actuel : {user?.name}</Text>
+        </View>
+    )
 }
 
 
